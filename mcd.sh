85b7ec2d1d8fee4d3a52c5d39140144bbd13b890
novalsaputra <novalsaputra0707@gmail.com>
2020-05-17 11:33:24 +0700

    edit

--- mcd.sh
+++ mcd.sh
@@ -1,10 +1,8 @@
 mcd(){
-	apt install vim
-	apt intall neofetch
 	mkdir -p "$1"
 	echo mantap_men > "$2.txt"
 }
-echo "berisi fungsi"
 
-# cara memanggil fungsi
-# mcd namaFolder namaFile
+apt install neofetch
+echo "tes install lewat bash"
+
